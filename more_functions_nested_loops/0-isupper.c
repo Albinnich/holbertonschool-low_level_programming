98b318c049f0365b5169bc6974914fd029536860
Albinnich <albina_haji@hotmail.com>
2024-02-26 08:20:56 +0000

    0. isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,13 @@
  * Returns 0 otherwise
  */
 
-int _isupper(int c);
-
+int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+
+	if (c >= 'A' && c <= 'Z')
 	{
-		return (1);
+	return (1);
 	}
+
 	return (0);
 }
