f4a81825697b4afe784ad2cdfffa885775eddbbb
Albinnich <albina_haji@hotmail.com>
2024-02-27 09:22:57 +0000

    Edit code

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,14 +3,14 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n - is parameter
+ * @n: parameter
  * Return: returns nothing
  */
 
 void print_line(int n)
 
 {
-	if (n-- > 0)
+	while (n-- > 0)
 	{
 		_putchar('_');
 	}
