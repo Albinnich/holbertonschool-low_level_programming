b6f7b8ed312dc8c7a5f0d7745acfc47af99bc079
Albinnich <albina_haji@hotmail.com>
2024-02-27 07:47:51 +0000

    Changing the structure of code

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,15 @@ int a, b;
 
 for (a = 0; a < 10; a++)
 {
-for (b = 0; b < 15; b++)
-{
-if (b > 9){
-_putchar (b / 10 + '0');
-}
-_putchar (b % 10 + '0');
-_putchar ('\n');
-}
+	for (b = 0; b < 15; b++)
+	{	
+		if (b > 9)
+		{
+			_putchar (b / 10 + '0');
+		}	
+		_putchar (b % 10 + '0');
+	}
+	_putchar ('\n');
+	
 }
 }
