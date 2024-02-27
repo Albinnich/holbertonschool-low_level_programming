25c1420091573614b630bc7829620c1410fe97c7
Albinnich <albina_haji@hotmail.com>
2024-02-27 09:29:20 +0000

    Edit code

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * print_line - writes a straight line in the terminal
- * @n: parameter
- * Return: returns nothing
- */
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
 
 void print_line(int n)
-
 {
 	while (n-- > 0)
 	{
 		_putchar('_');
 	}
-	putchar('\n');
+_putchar('\n');
 }
