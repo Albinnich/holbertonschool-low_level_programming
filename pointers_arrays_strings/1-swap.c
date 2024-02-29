54207f2430eef636a3f07f9901e370a4fc3c1d70
Albinnich <albina_haji@hotmail.com>
2024-02-29 12:30:22 +0000

    Betty finalized

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,9 +3,8 @@
 
 /**
  * swap_int - function swaps the values of two integers
- * @a - first parameter
- * @b - second parameter
- * Return: nothing
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
  */
 
 void swap_int(int *a, int *b)
