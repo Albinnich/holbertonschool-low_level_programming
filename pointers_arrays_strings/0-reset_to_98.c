5806e80dff6a1537ae9bb513971a4ebfe43f7896
Albinnich <albina_haji@hotmail.com>
2024-02-29 12:12:17 +0000

    Initial code

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * reset_to_98 - function that takes a pointer to an int
---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/** swap_int(int *a, int *b) - swaps the values of two integers
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+int main(void)
+
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
