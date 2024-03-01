2a961706927933e14fcc260a34be0a9e42d9f54a
Albinnich <albina_haji@hotmail.com>
2024-03-01 07:28:18 +0000

    Initial code

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _strlen - returns the length of a string
+ * @s: is a string
+ * return: returns length;
+ */
+
+int _strlen(char *s)
+
+{
+	int length = 0;
+	while (length != '\0')
+	length++;
+	return (length + 1);
+}
