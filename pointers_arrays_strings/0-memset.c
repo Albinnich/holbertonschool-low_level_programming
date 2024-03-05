e064c5ad1edca38ad6140b82537f73271e32fd83
Albinnich <albina_haji@hotmail.com>
2024-03-05 19:05:23 +0000

    Rewrote code

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,26 +1,26 @@
 /*
  * File: 0-memset.c
- * Auth: Albina Ismayilova
+ * Auth: not
  */
 
 #include "main.h"
 
 /**
- * _memset - The _memset() function fills
- * the first n bytes of memory area
- * pointed to by s with the constant byte b
- * @n: number of bytes
- * @b: constant byte
- * @s: target
- * Returns: returns new value of target
- */
+*_memset - The _memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
 
-	char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-		while (n)
-		{
-			s[n - 1] = b;
-			n--;
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
 	}
 	return (s);
 }
