067a33da4f4d07a275646e4960fd1a7cb76ecdcb
Albinnich <albina_haji@hotmail.com>
2024-03-05 19:34:18 +0000

    Edited

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -21,3 +24,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
+#endif /* MAIN_H */
