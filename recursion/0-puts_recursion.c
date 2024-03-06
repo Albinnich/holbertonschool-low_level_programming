72fee6b9a48c42ab59796507260c2946ae3e0b6d
Albinnich <albina_haji@hotmail.com>
2024-03-06 10:24:54 +0000

    Initial code

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - The _puts_recursion function prints a string
+ * @s: string
+ * Return: nothing returns
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(char);
+
+	return (0);
+}
