6b2fc36d51aa178fd73df6059e4d41fcdeb0afd6
Albinnich <albina_haji@hotmail.com>
2024-03-07 18:32:16 +0000

    Edited code

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,18 +3,18 @@
 /**
  * _puts_recursion - The _puts_recursion function prints a string
  * @s: string
- * Return: on success 1.
- * On error, -1 is returned, and errno is set correctly.
+ * Return: nothing.
  */
 
 void _puts_recursion(char *s)
 {
 
-	if (*s == 0)
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
-	_puts_recursion(s + 1);
+	s++;
+	_puts_recursion(s);
 }
