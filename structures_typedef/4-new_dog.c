59ec6313e09eb9b57230543950db622074d7dae3
Albinnich <albina_haji@hotmail.com>
2024-03-14 12:23:13 +0000

    Fixed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,16 +12,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *x;
+	int nlen, olen, i;
+	dog_t *doggy;
 
-	x = malloc(sizeof(dog_t));
-
-	if (x == NULL)
+	nlen = olen = 0;
+	while (name[nlen++])
+		;
+	while (owner[olen++])
+		;
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
 		return (NULL);
 
-	x->name = name;
-	x->age = age;
-	x->owner = owner;
+	doggy->name = malloc(nlen * sizeof(doggy->name));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < nlen; i++)
+		doggy->name[i] = name[i];
 
-	return (x);
+	doggy->age = age;
+
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < olen; i++)
+		doggy->owner[i] = owner[i];
+	return (doggy);
 }
