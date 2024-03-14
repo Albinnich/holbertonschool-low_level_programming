011d4709237a48834c0c6298266935030dcade22
Albinnich <albina_haji@hotmail.com>
2024-03-14 12:11:35 +0000

    Fixed

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,13 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - create dog.
+  * @name: name of dog.
+  * @age: age of dog.
+  * @owner: owner of dog.
+  * Return: 0
+  */
+
 struct dog
 
 {
@@ -6,9 +16,9 @@ struct dog
 	char *owner;
 };
 
-int main(void)
-{
-	struct dog user;
-
-	return (0);
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
