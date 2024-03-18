8e8408d2d598535094027a86722f81c59a7ae894
Albinnich <albina_haji@hotmail.com>
2024-03-18 21:03:04 +0000

    Initial

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Function array_iterator executes
+ * a function given as a parameter
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+	size_t i;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
