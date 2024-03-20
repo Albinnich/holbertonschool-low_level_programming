1a1bd146d8805cdfe1be9eb02e8c60dbe5d901a3
Albinnich <albina_haji@hotmail.com>
2024-03-20 18:27:10 +0000

    Initial

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function int_index searches for an integer
+ * @size: number of elements in array
+ * @array: array
+ * @cmp: pointer to the function
+ * Return: -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		cmp(array[i]);
+}
+	if (size <= 0)
+	{
+		return (-1);
+}
