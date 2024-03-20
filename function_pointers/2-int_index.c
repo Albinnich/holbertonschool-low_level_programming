f80d0873165eeb2adcf905d515791199479c9826
Albinnich <albina_haji@hotmail.com>
2024-03-20 19:04:44 +0000

    Changed

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,11 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; i > size; i++)
-		if (array != NULL && cmp != NULL)
-		{
+		if (size < 1 || array == NULL && cmp == NULL)
 			return (-1);
-		}
 
 	for (i = 0; i < size; i++)
 	{
