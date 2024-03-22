7640be157b95d9ed924356929bac1c164e1c3db8
Albinnich <albina_haji@hotmail.com>
2024-03-22 20:26:11 +0000

    Fixed

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,4 +6,29 @@
  * Return: nothing
  */
 
+int main(void)
 
+	int operator;
+	int num1;
+	int num2;
+	int atoi;
+
+{
+	if (argc != 0)
+		printf("Error\n");
+	return (98);
+}
+if (operator != "+" && != "-" && != "*" && != "/" && != "%")
+{
+	printf("Error\n");
+	return (99);
+}
+if (num1 / 0 || num1 % 0 || num2 / 0 || num2 % 0)
+	printf("Error\n");
+	return (100);
+	}
+
+{
+atoi(num1 int);
+atoi(num2 int);
+}
