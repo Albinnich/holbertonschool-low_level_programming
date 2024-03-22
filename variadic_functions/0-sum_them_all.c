08c35c5f20360e4b1d9ee70f480cb5419d6a67e1
Albinnich <albina_haji@hotmail.com>
2024-03-22 23:12:12 +0000

    Edited

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -16,16 +16,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	
 	for (i = 0; i < n; i++)
 	{
-	x = va_arg(args, int);
+	x = va_arg(args, const unsigned int);
 	sum += x;
 	}
 	va_end(args);
 	return (sum);
 	}
-	while (n == 0)
-	{
-	return (0);
-}
