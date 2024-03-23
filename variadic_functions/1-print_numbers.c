ea1a217ded209cb05d398d8cbe9bea73a40c8e9e
Albinnich <albina_haji@hotmail.com>
2024-03-23 11:58:56 +0000

    Fixed

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,9 +16,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(args, n);
+	va_arg(args, int);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", n);
+		printf("%d", va_arg(args, int));
 		if (separator != NULL && i < n - 1)
 	{
 		printf("%s", separator);
