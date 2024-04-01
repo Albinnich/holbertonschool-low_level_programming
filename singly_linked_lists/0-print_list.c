d2fa7f5061f80f07e118093feb2b27f3d310deb2
Albinnich <albina_haji@hotmail.com>
2024-04-01 19:30:42 +0000

    Initial code to print lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+
+{
+	const list_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+	if (current -> str != NULL)
+		printf("[%u] %s\n", current -> len, current -> str);
+	else
+		printf("[0] (nil)");
+	current = current -> next;
+	count++;
+	}
+	return (count);
+}
