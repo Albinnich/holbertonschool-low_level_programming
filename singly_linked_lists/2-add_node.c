b00ecdd6799ef1bb95ff938f7c89805ad5fa2a70
Albinnich <albina_haji@hotmail.com>
2024-04-03 19:26:30 +0000

    Initial try

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+#include <string.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * add_node - function to add new node at beginning of list
@@ -11,5 +12,25 @@
 list_t *add_node(list_t **head, const char *str)
 
 {
+	list_t *new_node;
 
-	const char *str = strdup(
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+		new_node->str = strdup(str);
+		if (new_node->str == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+
+		new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
