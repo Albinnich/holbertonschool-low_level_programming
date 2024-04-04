a1e68ee17225bca0f783d948c186c041c1322be2
Albinnich <albina_haji@hotmail.com>
2024-04-04 11:30:26 +0000

    Initial try

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * dlistint_len - function that returns number of elements
+ * @h: head node
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+
+{
+	const dlistint_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
