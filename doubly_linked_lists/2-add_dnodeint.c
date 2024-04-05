e1a08ecfd417ae49602bdb38e917eac6d7278bcd
Albinnich <albina_haji@hotmail.com>
2024-04-05 09:18:50 +0000

    Code changed

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * dlistint_t - function that adds a new node at start
+ * add_dnodeint - function that adds a new node at start
  * @head: head node
  * @n: integer
  * Return: address of new element or NULL if failed
@@ -12,13 +13,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 {
 	dlistint_t *new_node;
+
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	if (new_node == NULL);
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
@@ -32,24 +34,4 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	*head = new_node;
 	return (new_node);
-}
-
-void print_dlistint(dlistint_t *head)
-{
-	while (head != NULL)
-	{
-		printf("%d ", head->n);
-		head = head->next;
-	}
-	printf("\n");
-}
-void free_distint(dlistint_t *head)
-{
-	dlistint_t *temp;
-	while (head != NULL)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
 }
