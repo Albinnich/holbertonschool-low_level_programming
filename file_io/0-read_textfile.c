ce0429e6644efe05c40c0c71f3ab9441574a907b
Albinnich <albina_haji@hotmail.com>
2024-04-07 07:58:41 +0000

    Creating files

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads text file
+ * and prints it to POSIX stdout
+ * @filename: name of file
+ * @letters: number of letters
+ * Return: 0 if file can't be opened or read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+
+{
+
